dbff6a279c962ca609220e2b002529793a958491
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-05-04 21:58:05 -0400

    Completed assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function Toggle() {
-  return <button>OFF</button>;
+   /*
+Thinking in React
+  - Is data passed as a prop? If so, no state
+  - Can you compute data based on any other state or props in your component? If so, no state
+  - Does data remain unchanged over time? If so, no state
+*/
+   const [toggle, setToggle] = useState(false);
+   function handleClick() {
+      let newToggle = !toggle;
+      setToggle(newToggle); // my solution
+      // setToggle((toggle) => !toggle) // Canvas solution
+   }
+   const color = toggle ? 'red' : 'white';
+
+   return (
+      <button style={{ background: color }} onClick={handleClick}>
+         {toggle ? 'ON' : 'OFF'}{' '}
+      </button>
+   );
 }
 
 export default Toggle;
